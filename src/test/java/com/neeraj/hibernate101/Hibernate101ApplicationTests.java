a62a8e9af946dd9188ec77c409e2a4dfd1faeb0d
neeraj gupta <neeraj.gupta@halodoc.com>
2021-04-02 17:29:03 +0530

    Added working code

---
+++ src/test/java/com/neeraj/hibernate101/Hibernate101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neeraj.hibernate101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hibernate101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
